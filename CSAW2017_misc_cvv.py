184308c05a49ee060eb910cc2ce243933cca36f1
Muh. Fani Akbar <rhamaa@users.noreply.github.com>
2017-09-17 13:55:24 +0700

    Update CSAW2017_misc_cvv.py

--- CSAW2017_misc_cvv.py
+++ CSAW2017_misc_cvv.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python
+# Credit to : https://stackoverflow.com/questions/21079439/implementation-of-luhn-formula
+# Credit to : https://github.com/grahamking/darkcoding-credit-card
+
+"""
+gencc: A simple program to generate credit card numbers that pass the
+MOD 10 check (Luhn formula).
+Usefull for testing e-commerce sites during development.
+Copyright 2003-2012 Graham King
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
 
 # Flag
 # flag{ch3ck-exp3rian-dat3-b3for3-us3}
